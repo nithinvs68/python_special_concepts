6032472fc7fb20f5fa5aeac608afad5b1b23f614
nithin v s <nr49005@gmail.com>
2024-01-02 12:19:28 +0530

    Abstract Class

---
+++ abstract_base_class.py
@@ -0,0 +1,28 @@
+from abc import ABC,abstractmethod
+
+class Team(ABC):
+    @abstractmethod
+    def diet(self):
+        pass
+
+    @abstractmethod
+    def walk(self):
+        pass
+
+    def behavior(self):
+        print("They show complex expression and social nehaviour")
+
+class Human(Team):
+    def diet(self):
+        #pass, Will get error if no implementation for this function since it is abstract in base class
+        print("Human are carnivorous")
+
+    def walk(self):
+        # pass ,Will get error if no implementation for this function since it is abstract in base class
+        print("They are bipeds")
+
+nithin=Human()
+print(nithin.diet())
+print(nithin.walk())
+print(nithin.behavior())
+help(ABC)
